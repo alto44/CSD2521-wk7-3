3ba059d86a0dc15f09c63f227c968ea5d3666607
connd7883 <89489286+connd7883@users.noreply.github.com>
2021-10-07 22:59:56 -0400

    Update HelloTest1.java
    - line 3:     Added comment containing Updated date;
    - line 9 :    Updated comment;
    - line 13:   Updated println() statement;

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,14 +1,15 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/07/2021 by DGC
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * I can follow the steps easy enough but have no idea what I'm doing!!!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
-        System.out.println("Test 3");
+        System.out.println("To Test 3");
         }
     }
